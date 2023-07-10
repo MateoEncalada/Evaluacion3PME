8708f5c181b6def0bf16484566acfb223c89c7f3
Mateo Encalada <mateo.encalada@udla.edu.ec>
2023-07-10 08:30:21 -0500

    Intento API

---
+++ MVVM/ViewModel/ClaseViewModelME.cs
@@ -0,0 +1,23 @@
+﻿using Evaluacion3PME.MVVM.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Evaluacion3PME.MVVM.ViewModel
+{
+    public class ClaseViewModelME
+    {
+        public ClaseME ClaseME { get; set; }
+        public ClaseViewModelME()
+        {   
+            ClaseME = new ClaseME();
+            {
+                   
+                    
+            };
+        }
+    }
+}
